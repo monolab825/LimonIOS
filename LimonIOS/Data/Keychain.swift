dd052138805c109e1c8734d70ed0da2c498117da
monolab825 <p.smiles777@gmail.com>
2024-05-20 12:49:14 +0200

    WIP Added keychain flow

---
+++ LimonIOS/Data/Keychain.swift
@@ -0,0 +1,41 @@
+//
+//  Keychain.swift
+//  LimonIOS
+//
+//  Created by Marcos on 20/5/24.
+//
+
+import Foundation
+import KeychainSwift
+
+protocol KeychainProtocol {
+    func save(key: String, value: String)
+    func getKey(key: String) -> String
+    func deleteKey(key: String)
+}
+
+
+final class Keychain: KeychainProtocol {
+    private let keychain = KeychainSwift()
+    private let shared = Keychain()
+    
+    
+    func save(key: String, value: String) {
+        keychain.set(value, forKey: key)
+    }
+    
+    func getKey(key: String) -> String {
+        guard let value = keychain.get(key) else {
+            return "Key not found"
+        }
+        
+        return value
+    }
+    
+    func deleteKey(key: String) {
+        keychain.delete(key)
+    }
+    
+
+    
+}
